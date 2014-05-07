767a679d4c94cc78c967e8ce9efcd7ebab6fd32c
Mike Evans <mikee@millstreamcomputing.co.uk>
2014-05-07 12:57:30 +0100

    new file:   ebay2bill.py

---
+++ ebay2bill.py
@@ -0,0 +1,4 @@
+# Create a GnuCash bill froman e-bay e-mail confirmation.
+# In my case these are always paid using PayPal so consider them paid.
+# The transfer account varias though so perhaps don't.
+
