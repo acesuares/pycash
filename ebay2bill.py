6749a34a1b53c63065644239a6e0438f3dc37e61
Mike Evans <mikee@millstreamcomputing.co.uk>
2014-05-15 12:36:42 +0100

    Fixed problem with duplicate attributes.
    Assigning values to Class attributes is wrong.  Values need to be
    assigned to Object attributes.  Define these in the __init__() function
    NOT in the Class body.  Oops!

--- ebay2bill.py
+++ ebay2bill.py
@@ -16,63 +16,41 @@ import email
 import json
 import copy
 import logging
+import uuid
 
 logging.basicConfig(level=logging.DEBUG, format='%(module)s: LINE %(lineno)d: %(levelname)s: %(message)s')
 
 class Vendor:
+    guid = None
     name = ''
     addr = []
     email = ''
     items = [] # List of items from this vendor.
-        
+
     def __init__(self):
+        self.guid = uuid.uuid4()
         return
-         
-    '''
-    def parse_seller_information(self,plain):
-        foo = plain.split("\n")
-        seller = ""
-        #print foo
-        item_dict = {}
-        item_list = []
-        for line in foo:   
-            if line.find("Seller:") != -1:
-                seller_dict = {}
-                seller = line.rpartition('Seller:')[2].lstrip(' ')
-                #seller_dict.update({'Seller':seller}) ## Just the name. we need address
-                seller_dict.update({'Addr1':seller}) # Dummy address
-                seller_dict.update({'Addr2':"2 Addr Dummy"}) # Dummy address
-                self.sellers.update({seller:seller_dict}) # Add to the sellers dict
-                self.items.append(seller)
-            else:
-                for needle in self.item_search_terms:
-                    if line.find(needle) != -1:
-                       
-                        item_dict.update({line.rpartition(needle)[1].strip(':'):\
-                            line.rpartition(needle)[2].lstrip(' ').decode('ascii','ignore').encode('utf8')})
-                        self.items.append( line.rpartition(needle)[2].lstrip(' ').decode('ascii','ignore').encode('utf8'))
-                #item_dict.update({seller:self.items})
-        print 
-    '''
-            
+
     def parse_vendor_data(self, data):
         return
-        
+
     def add_item(self, item):
         self.items.append(item)
-        
+
     def print_vendor(self):
         print self.name
         for add in self.addr:
             print add
-        
 
-        
+
+
 ###### END CLASS VENDOR ########
 
 
 class Item:
-    attribs = {}
+    # Class attributes
+    attribs = None
+    guid = None
     item_search_terms=( "Item name", #
                         "Item number:", #
                         "transaction::",  #
@@ -81,47 +59,58 @@ class Item:
                         "Quantity:", #
                         "Item total:",  #
                         "Paid on")  #
-    
+
     def __init__(self, item_data):
+        # Object attributes
+        self.attribs = {}
+        self.guid = uuid.uuid4()
+        # Do stuff
         self.parse_item_information(item_data)
-        
-    def parse_item_information(self,plain):
-        for line in plain:
+
+    def parse_item_information(self,data):
+        for line in data:
             for needle in self.item_search_terms:
                 if line.find(needle) != -1:
                     self.attribs[needle.strip(':')] = line.rpartition(needle)[2].lstrip(' ')
-                    
-        
+
     def print_items(self):
         #print self.attribs.items()
         for key, value in self.attribs.items():
             print key, '->', value
-    
+
     def get_attribs(self):
         return attribs
-        
-#### END CLASS ITEM #######
-                    
 
+#### END CLASS ITEM #######
 
 
 class Purchase:
     ''' A purchase from a single vendor and may be one or more items.
-    Probably should be called purchase? 
+    Probably should be called purchase?
     '''
-    items = [] # A Purchase has one or more items
-    vendor = Vendor() # from a single vendor
-    
+    items = None # A Purchase has one or more items
+    vendor = None # from a single vendor
+    purchase_data = None
+    guid = None
+
     def __init__(self, purchase_data):
-        #logging.info("Purchase\n\n%s",str(purchase_data)) ## DEBUG
-        self.parse_vendor(purchase_data)
-        self.parse_purchase(purchase_data)
-        #self.print_purchase() # DEBUG
-                
+        self.guid = uuid.uuid4()
+        self.vendor =  Vendor()
+        self.items = []
+        self.purchase_data =  purchase_data
+        if not type(purchase_data) == list: return
+        #logging.info("\n\n"+str(self.guid)+"\n"+str(self.purchase_data)) ## DEBUG
+        self.parse_vendor(self.purchase_data)
+        self.parse_purchase(self.purchase_data)
+
+    def parse_vendor(self, data):
+        self.vendor.name=data[0].rpartition('Seller:')[2].lstrip(' ') # Always
+        for i in range(4,8):
+            if data[i][0:3] == '---': break ## See mail.txt for why
+            self.vendor.addr.append(data[i].lstrip(' ').rstrip(' '))
+
     def parse_purchase(self, data):
         idxs = []
-        #print '\n\n'
-        #self.vendor.print_vendor()
         for i in range(len(data)-1):
             if data[i].find('Item name ') != -1:
                 idxs.append(i)
@@ -129,17 +118,8 @@ class Purchase:
         for i in range(len(idxs)-1):
             item = Item(data[idxs[i]:idxs[i+1]])
             self.items.append(item)
-            #print'\n'
-            #item.print_items()           
         return
-        
-        
-    def parse_vendor(self, data):
-        self.vendor.name=data[0].rpartition('Seller:')[2].lstrip(' ') # Always
-        for i in range(4,8):
-            if data[i][0:3] == '---': break
-            self.vendor.addr.append(data[i].lstrip(' ').rstrip(' '))
-        
+
     def print_purchase(self):
         print '\nVendor Data:\n'
         self.vendor.print_vendor()
@@ -147,26 +127,26 @@ class Purchase:
         for item in self.items:
             item.print_items()
         return
-        
+
     def get_items(self):
         return self.items
-        
+
     def get_vendor(self):
         return self.vendor
 
-###### END CLASS Purchase ########    
-    
-    
+###### END CLASS Purchase ########
+
+
 class EbayMail():
     purchases = [] # List of Purchases in email, usually just the one but mutiples are possible.
-     
+
     def __init__(self,billmail, cashfile, account):
         self.INFILE = billmail
         self.account = account
         f = open(self.INFILE)
         self.msg = email.message_from_file(f)
         f.close()
-        
+
     def get_plain_mail(self,mail):
         ''' Get the plain text part of the mail.'''
         for part in mail.walk():
@@ -174,7 +154,7 @@ class EbayMail():
             # that contains further parts... Message is organized like a tree
             if part.get_content_type() == 'text/plain':
                 return part.get_payload(None, True) # Make a string of the text.
-     
+
     def parse_mail(self,plist):
         ''' Parse the mail.
         Collect the indices of where the serch terms occur, then split the file
@@ -189,45 +169,49 @@ class EbayMail():
                 #print line
                 idxs.append(plist.index(line))
         #logging.info(str(idxs))
-        for i in range(len(idxs)-1):           
+        # Each section we have defined above is a purchase
+        for i in range(len(idxs)-1):
             purchase = Purchase(plist[idxs[i]:idxs[i+1]])
             self.purchases.append(purchase)
-            pass
-        for p in self.purchases:
-            print '\n\n'
-            p.print_purchase()
-        return  
-        
+
     def make_invoice(self):
         session = bill.Session("example.gnucash")
         session.open()
         print session.vendor_search("E Bay", 100)
-        session.close()    
+        session.close()
         return
-    
+
     def parse_totals(self,plain):
-        
+
         return
-                    
+
 ##### END CLASS EBAYBILL ############
 
 
-    
+
 # START HERE
 if __name__ == "__main__":
     VENDOR_ID="000019" # All ebay stuff in here.
 
 try: GNUFILE=sys.argv[1]
 except: GNUFILE="example.gnucash"
-	
+
 try: ACCOUNT=sys.argv[2]
 except:	ACCOUNT="Business Expenses" # Default if absent on the command line.  Edit to suit your account tree
 
 try: MAILFILE = sys.argv[3]
 except: MAILFILE = "Confirmation of your order of Voltage Regulator LM7805 LM7812 LM317T Adjustable Linear 7805 7812 UK..."
-    
+
 ebay_mail = EbayMail(MAILFILE,GNUFILE, ACCOUNT)
 plain = ebay_mail.get_plain_mail(ebay_mail.msg)
 plist = plain.lstrip(' ').decode('ascii','ignore').encode('utf8').split('\n')
 ebay_mail.parse_mail(plist)
+for p in ebay_mail.purchases:
+    print p.guid
+    #print p.purchase_data
+    print "\tVendor -",p.vendor.guid, p.vendor.name
+    for i in p.items:
+        print "\tItem -",i.guid, i.attribs['Item name']
+
+    #p.print_purchase()
 
