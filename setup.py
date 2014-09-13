55e34a96570ffa32cfbda2ea1036df9e6e63032c
Mike Evans <mikee@saxicola.co.uk>
2014-09-13 14:57:59 +0100

    Squashed 'ebay2gnuCash/' content from commit 3b52a38
    git-subtree-dir: ebay2gnuCash
    git-subtree-split: 3b52a38b22c2d0aae553750b33c9e8a4c3b14a63

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Ebay2GnuCash",
+    version = "0.1",
+    packages=['ebay2cash'],
+    package_dir={'ebay2cash': 'src'},
+    author = "Mike Evans",
+    author_email = "mikee@mutant-ant.com",
+    description = "This is an Example Package",
+)
